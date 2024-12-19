21343e98667c0caf9e4b6e36502b460f1df0340c
Jayan Nimna Chethiya <94435820+Chethiya99@users.noreply.github.com>
2024-12-19 14:32:00 +0530

    Update Pulse_main.py

--- Pulse_main.py
+++ Pulse_main.py
@@ -1,7 +1,11 @@
-import os
-import json
+#import os
+#import json
 
-import pysqlite3 as sqlite3
+#import pysqlite3 as sqlite3
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 
 from pathlib import Path
 from datetime import datetime, timezone
